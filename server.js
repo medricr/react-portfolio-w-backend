fcb90fd5947e047c20e8cb25cd425a1e116e5dc5
Medric Riley <medricr@gmail.com>
2020-05-15 17:44:31 -0700

    added nodemailer to node_modules

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/contact', (req,res)=> {
+	const stmpTrans = nodemailer.createTransport({
+		// set up transporter here
+	})
+})
 
 app.listen(PORT, ()=> {
 	console.log("app listening on port " + PORT)
