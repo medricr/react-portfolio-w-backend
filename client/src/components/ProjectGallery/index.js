0b68ea5dfbffeb90b30c1dd5ef06667dd9734277
Medric Riley <medricr@gmail.com>
2020-06-22 13:43:50 -0700

    chenged project page

--- client/src/components/ProjectGallery/index.js
+++ client/src/components/ProjectGallery/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 // IMAGES
 import keyboard from '../../assets/keyboard.jpg'; //Photo by Caspar Camille Rubin on Unsplash
-import record from '../../assets/record.jpg'; //Photo by Ivan Dorofeev on Unsplash
+// import record from '../../assets/record.jpg'; //Photo by Ivan Dorofeev on Unsplash
 import note from '../../assets/stickynote.jpg'; //Photo by Paper Textures on Unsplash
 import speaker from '../../assets/speaker.jpg'; //Photo by Josh Sorenson on Unsplash
+import checklist from '../../assets/checklist.jpg'; //Photo by Glenn Carsten-Peters on Unsplash
 // COMPONENTS
 import ProjectCard from'../ProjectCard';
 
@@ -14,32 +15,8 @@ class ProjectGallery extends React.Component {
 				<div className="row">
 					<div className="col-md-12">
 						<div className="jumbotron rounded mt-5">
-							{/* <div className='row' id="projectRow">
+							<div className='row' id="projectRow">
 								<div className='col-md-2' />
-								
-								
-								<div className="col-md-4">
-									<ProjectCard
-										projectTitle="MongoDB Web Scraper"
-										projectSubtitle="A web scraping app using mognoose, node.js, and handlebars."
-										projectDescription="This simple, single page application uses node.js to gather all articles from a webpage (in this case, Pitfork.com's album review page). The article links, titles, and descriptions are then placed into a mongoose database, which is then used to pull all that information and and put it onto the webpage."
-										projectLink="https://mongo-web-scraperr.herokuapp.com/"
-										projectImage={record}
-									/>
-								</div>
-								<div className='col-md-2' />
-							</div> */}
-							<div className='row'>
-								{/* <div className='col-md-2' /> */}
-								<div className='col-md-4'>
-									<ProjectCard 
-										projectTitle="LIRI"
-										projectSubtitle="An API based application which uses the command line to execute different scripts."
-										projectDescription="This simple command line application uses several different APIs; Spotify, Bandsintown, and the OMDB, to return information about movies, songs, or bands that the user enters. Using a set of pre-defined commands, the user can return the top five results from spotify when searching a song, the next five shows that a band is playing, or the top result from the OMDB. In addition, the applicaiton can read a command from a text file and execute that command."
-										projectLink="https://github.com/medricr/LIRI_Bot"
-										projectImage={speaker}
-									/>
-								</div>
 								<div className="col-md-4">
 									<ProjectCard
 										projectTitle="MERN Boilerplate"
@@ -49,6 +26,30 @@ class ProjectGallery extends React.Component {
 										projectImage={note}
 									/>
 								</div>
+								
+								<div className="col-md-4">
+									<ProjectCard
+										projectTitle="EDD Progress Tracker"
+										projectSubtitle="A logging application to track ones progress by call with the EDD phone lines. "
+										projectDescription="Half joke, half proof of concept, this project uses the previous set of MERN boilerplate code as a starting point to create a simple logging app which the user can use to track their progress on the phone with the EDD. Complete with create, read, update, and delete functionality for each day of calls, these collections are tied to each individual user's account"
+										projectLink="https://edd-progress-tracker.herokuapp.com/"
+										projectImage={checklist}
+									/>
+								</div>
+								<div className='col-md-2' />
+							</div>
+							<div className='row'>
+								<div className='col-md-2' />
+								<div className='col-md-4'>
+									<ProjectCard 
+										projectTitle="LIRI"
+										projectSubtitle="An API based application which uses the command line to execute different scripts."
+										projectDescription="This simple command line application uses several different APIs; Spotify, Bandsintown, and the OMDB, to return information about movies, songs, or bands that the user enters. Using a set of pre-defined commands, the user can return the top five results from spotify when searching a song, the next five shows that a band is playing, or the top result from the OMDB. In addition, the applicaiton can read a command from a text file and execute that command."
+										projectLink="https://github.com/medricr/LIRI_Bot"
+										projectImage={speaker}
+									/>
+								</div>
+								
 								<div className="col-md-4">
 									<ProjectCard
 										projectTitle="Inventory Manager"
@@ -58,7 +59,7 @@ class ProjectGallery extends React.Component {
 										projectImage={keyboard}
 									/>
 								</div>	
-								{/* <div className='col-md-1' />							 */}
+								<div className='col-md-2' />							
 							</div>
 						</div>
 					</div>
